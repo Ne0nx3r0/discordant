f8230c58cd63db39c51c0a0053a9d6cdbc2d9562
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-01-21 23:05:47 -0600

    battle

--- src/bot/commands/Battle.ts
+++ src/bot/commands/Battle.ts
@@ -24,44 +24,7 @@ export default class Battle extends Command{
     }
 
     run(params:Array<string>,message:any,game:Game){
-        if(params.length < 1){
-            message.reply('You must choose a class\n\n'+this.getAvailableClasses());
+        
 
-            return;
-        }
-
-        const existingPlayer = game.getPlayerCharacter(message.author.id);
-
-        if(!existingPlayer){
-            message.reply('You have already begun');
-
-            return;
-        }
-
-        const wantedClassStr = params[0];
-        const wantedClassStrUpper = wantedClassStr.toUpperCase();
-
-        const wantedClass:CharacterClass = CharacterClasses.find((val)=>{
-            return val.title.toUpperCase() == wantedClassStrUpper;
-        });
-
-        if(!wantedClass){
-            message.reply(params[0]+' is not a valid class\n\n'+this.getAvailableClasses());
-
-            return;
-        }
-
-        game.registerPlayerCharacter({
-            uid: message.author.id,
-            discriminator: message.author.discriminator,
-            username: message.author.username,
-            class: wantedClass
-        })
-        .then(function(pc){
-            message.reply('You have successfully been registered as a '+wantedClass.title+', good luck!');
-        })
-        .catch(function(err){
-            message.reply(err);
-        });
     }
 }
