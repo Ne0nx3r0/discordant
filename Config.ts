bd9fdbd1829cd4b1484726d664f8e3a1ba3d32dd
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-01-23 13:55:35 -0600

    FML

--- Config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    GameBotAuthToken: 'MjYzMDMzMjY2NDY0ODgyNjg4.C0MIaw.Ov2YJWRZk97DuJU4t4tzYAex6wc',
-    DatabaseConfig: {
-        host: '192.168.1.12',
-        user: 'discordant',
-        password: 'qDR6EaVOEFc7dEOVB2HhgQ9U8',
-        database: 'discordant'
-    }
-};
