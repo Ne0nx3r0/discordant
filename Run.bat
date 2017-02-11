4db1ff02bc250f114579cf879d39105a7e964250
Ne0nx3r0 <ne0nx3r0@gmail.com>
2017-02-10 21:13:06 -0600

    autorestart

--- Run.bat
+++ Run.bat
@@ -1,3 +1,15 @@
-:begin
+:start
+
+echo Restarting in ten seconds...
+
+ping -n 10 127.0.0.1 > nul
+
+echo 'Pulling fresh code'
+
+git pull
+
+echo 'Running npm start'
+
 call npm start
-GOTO begin
+
+goto start
